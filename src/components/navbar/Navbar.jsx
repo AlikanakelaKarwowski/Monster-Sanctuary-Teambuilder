14c1df2b80d47184d03ff18ecec32c46619c4282
A.Karwowski <akk42@zips.uakron.edu>
2021-12-31 20:31:31 -0700

    Start Navbar setup

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,8 +1,16 @@
 import React from 'react'
+import {RiMenu3Line, RiCloseLin} from 'react-icons/ri'
+import logo from '../../assets/MS_logo.png'
 import './navbar.css'
+
+//BEM -> Block Element Modifier
 const Navbar = () => {
     return (
-        <div>
+        <div className="ms__navbar">
+            <div className="ms__navbar-links"></div>
+                <div className="ms__navbar-links_logo">
+                <img src={logo} alt="Logo" />
+                </div>
             Navbar
         </div>
     )
