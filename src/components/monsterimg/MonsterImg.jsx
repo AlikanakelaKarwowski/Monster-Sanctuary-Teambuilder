0d40dd1afd67d97a8b2bea4f24bd89dcd59f5edd
A.Karwowski <akk42@zips.uakron.edu>
2022-01-05 18:45:11 -0700

    init monsterimg componet files

---
+++ src/components/monsterimg/MonsterImg.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import './monsterimg.css'
+import Monster from '../../assets/Spectral_Toad.png'
+const MonsterImg = () => {
+    return (
+        <div>
+            Monster Image here
+            <img src = {Monster} />
+        </div>
+    )
+}
+
+export default MonsterImg
